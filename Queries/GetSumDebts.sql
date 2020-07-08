7a17541aef8db2438644a23b621baf1d4db2f9af
davi <davi.b.souza@zanc.com.br>
2020-07-08 20:18:47 -0300

    erros no sql

--- Queries/GetSumDebts.sql
+++ Queries/GetSumDebts.sql
@@ -1,1 +1,1 @@
-select sum(valor) from caddiv WHERE iscardcred = 'false' and EXTRACT(MONTH FROM vencimento) = {} and EXTRACT(YEAR FROM vencimento) = {} or tipodedivida = 'fixa'
+select sum(valor) from caddiv WHERE EXTRACT(MONTH FROM vencimento) = {} and EXTRACT(YEAR FROM vencimento) = {} or tipodedivida = 'fixa'
